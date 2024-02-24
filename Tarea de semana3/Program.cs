6b967b7f1ffef5a7c1d89d54f6b12b75ef916c99
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-02-24 16:11:56 -0600

    Tercer ejercicio

--- Tarea de semana3/Program.cs
+++ Tarea de semana3/Program.cs
@@ -1,4 +1,5 @@
-﻿//try
+﻿// EJERCICIO 1
+//try
 //{
 //    Console.WriteLine("Ingrese primer numero");
 //    int num1 = int.Parse(Console.ReadLine());
@@ -28,22 +29,52 @@
 //    Console.WriteLine("Vuelve a intentar");
 //}
 
+//EJERCICIO 2
+//try
+//{
+//    Console.WriteLine("Bienvenido al club");
+//    Console.WriteLine("Coloque su edad para continuar");
+//    int edad = int.Parse(Console.ReadLine());
+
+//    if (edad >= 18)
+//    {
+//        Console.WriteLine("Bienvenido eres mayor de edad y puedes entrar");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Lo siento, eres menor de edad así que no puedes entrar");
+//    }
+//}
+//catch (Exception ex)
+//    {
+//    Console.WriteLine("No ingresaste un valor valido (SOLO NUMEROS)");
+//    }
+
+//EJERCICIO 3
 try
 {
-    Console.WriteLine("Bienvenido al club");
-    Console.WriteLine("Coloque su edad para continuar");
-    int edad = int.Parse(Console.ReadLine());
 
-    if (edad >= 18)
+    Console.WriteLine("Bienvenido a descuentos");
+    Console.WriteLine("Agregue el precio del producto");
+    double precio = double.Parse(Console.ReadLine());
+
+    if (precio > 100)
     {
-        Console.WriteLine("Bienvenido eres mayor de edad y puedes entrar");
+        Console.WriteLine("Si aplica al descuento");
+        double descuen = precio / 10;
+        double liquido = precio - descuen;
+
+        Console.WriteLine("Se aplico un descuento del 10% ");
+        Console.WriteLine("Su total a pagar es de: " + liquido);
     }
     else
     {
-        Console.WriteLine("Lo siento, eres menor de edad así que no puedes entrar");
+        Console.WriteLine("No aplicas para el descuento");
+
     }
+
 }
 catch (Exception ex)
-    {
+{
     Console.WriteLine("No ingresaste un valor valido (SOLO NUMEROS)");
-    }
+}
