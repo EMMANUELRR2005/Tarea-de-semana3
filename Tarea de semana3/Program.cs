c9e2f9b8473f64ecff346de632713b7a3f764d8f
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-02-24 15:57:37 -0600

    Segundo ejercicio

--- Tarea de semana3/Program.cs
+++ Tarea de semana3/Program.cs
@@ -1,29 +1,49 @@
-﻿try
+﻿//try
+//{
+//    Console.WriteLine("Ingrese primer numero");
+//    int num1 = int.Parse(Console.ReadLine());
+
+//    Console.WriteLine("Ingrese segundo numero");
+//    int num2 = int.Parse(Console.ReadLine());
+
+//    Console.WriteLine("Ingrese tercer numero");
+//    int num3 = int.Parse(Console.ReadLine());
+
+//    if (num1 > num2 && num1 >num3)
+//    {
+//    Console.WriteLine("El numero mayor es: " + num1);
+//    }
+//    if (num2 >num1 && num2 > num3)
+//    {
+//    Console.WriteLine("El numero mayor es: " + num2);
+//    }
+//    else
+//    {
+//    Console.WriteLine("El numero mayor es: " + num3);
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("No ingresaste un valor numerico");
+//    Console.WriteLine("Vuelve a intentar");
+//}
+
+try
 {
-    Console.WriteLine("Ingrese primer numero");
-    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Bienvenido al club");
+    Console.WriteLine("Coloque su edad para continuar");
+    int edad = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Ingrese segundo numero");
-    int num2 = int.Parse(Console.ReadLine());
-
-    Console.WriteLine("Ingrese tercer numero");
-    int num3 = int.Parse(Console.ReadLine());
-
-    if (num1 > num2 && num1 >num3)
+    if (edad >= 18)
     {
-    Console.WriteLine("El numero mayor es: " + num1);
-    }
-    if (num2 >num1 && num2 > num3)
-    {
-    Console.WriteLine("El numero mayor es: " + num2);
+        Console.WriteLine("Bienvenido eres mayor de edad y puedes entrar");
     }
     else
     {
-    Console.WriteLine("El numero mayor es: " + num3);
+        Console.WriteLine("Lo siento, eres menor de edad así que no puedes entrar");
     }
 }
 catch (Exception ex)
-{
-    Console.WriteLine("No ingresaste un valor numerico");
-    Console.WriteLine("Vuelve a intentar");
-}
+    {
+    Console.WriteLine("No ingresaste un valor valido (SOLO NUMEROS)");
+    }
